72005b90a32ad09201834aa4bf9b4c034efe3251
Abdullah Imran (BLR GSS) <abdullahimran@nousinfo.com>
2018-07-26 15:57:32 +0530

    Intial commit from DEV

--- src/com/Pacakge.java
+++ src/com/Pacakge.java
@@ -3,5 +3,6 @@ package com;
 public class Pacakge{
 	public static void main(String[] args) {
 		System.out.println("Hello World abduala");
+		System.out.println("Commiting changes from DEV branch");
 	}
 }
