9b4f3106ac859c2c5cc61f709eaed7e2869126d1
Abdullah Imran (BLR GSS) <abdullahimran@nousinfo.com>
2018-07-26 16:37:53 +0530

    reset/revert from 2nd commit

--- src/com/Pacakge.java
+++ src/com/Pacakge.java
@@ -3,5 +3,7 @@ package com;
 public class Pacakge{
 	public static void main(String[] args) {
 		System.out.println("Hello World abduala");
+		System.out.println("Commiting changes from DEV branch");
+		System.out.println("Abduala is chuitya doing in dev");
 	}
 }
